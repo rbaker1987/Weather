0a6a617c5aa3a02e0a7802128d96a6259553533b
rbaker <rbaker1987@gmail.com>
2022-12-17 15:43:34 -0600

    Added standard forecast call from NWS. Mass update to GUI.

--- CreateForecastUI.py
+++ CreateForecastUI.py
@@ -1,48 +1,78 @@
+import csv
 import datetime
-
+from NWS import NWS
 import PySimpleGUI as sg
 
 today = datetime.date.today()
+now = datetime.datetime.now()
 
 
 def new_layout(i):
     return [
-        [sg.T("AM Temp"), sg.InputText(key=("am-t", i), size=5), sg.T("PM Temp"), sg.InputText(key=("pm-t", i), size=5),
-         sg.T("Weather"), sg.InputText(key=("weather", i))]]
+        [sg.T('Date'), sg.InputText(today + datetime.timedelta(days=i + 1), key=('date', i), size=10),
+         sg.T("AM Temp"), sg.InputText(key=("am-t", i), size=5),
+         sg.T("PM Temp"), sg.InputText(key=("pm-t", i), size=5),
+         sg.T("Weather"), sg.InputText(key=("weather", i), size=30)]
+    ]
 
 
 column_layout = [
-    [sg.T("AM Temp"), sg.InputText(key="am-t", size=5), sg.T("PM Temp"), sg.InputText(key="pm-t", size=5),
-     sg.T("Weather"), sg.InputText(key="weather"), sg.Button('Add', enable_events=True, key="add")]
+    [sg.T('Date'), sg.InputText(today, key='date', size=10),
+     sg.T("AM Temp"), sg.InputText(key="am-t", size=5),
+     sg.T("PM Temp"), sg.InputText(key="pm-t", size=5),
+     sg.T("Weather"), sg.InputText(key="weather", size=30)]
 ]
 
-sg.theme('DarkAmber')  # Add a touch of color
-# All the stuff inside your window.
-layout = [[sg.Column(column_layout, key='column')], [sg.Submit()]]
+layout = [
+    [sg.T('Location'), sg.InputText('Lindale, TX', key='location')],
+    [sg.T('NWS Forecast:'), sg.Button('Load Forecast')],
+    [sg.Table(key='nws', values=[], headings=['  Date  ', 'Hour', 'Temp', '  Wind  ', '      Weather      '])],
+    [sg.T('Enter forecast for day:')],
+    [sg.Column(column_layout, key='column')],
+    [sg.Button('Add Day'), sg.Submit()],
+    [sg.T('Custom forecast:')],
+    [sg.Table(key='output', values=[], headings=['  Date  ', 'AM Temp', 'PM Temp', '      Weather      '])],
+    [sg.T('Save Location'), sg.InputText(key='save_as'), sg.SaveAs(target='save_as', default_extension='csv'),
+     sg.Button('Export')]
+]
 
-# Create the Window
 window = sg.Window('Create Forecast', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-i = 1
+
+create_forecast = []
+i = 0
 while True:
     event, values = window.read()
-    create_forecast = []
-    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+    location = values['location']
+    if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-    elif event == 'add':
-        f_date = today + datetime.timedelta(days=i-1)
-        f_row = {'created': today, 'date': f_date, 'temp_am': values['am-t'],
-                 'temp_pm': values['pm-t'], 'weather': values['weather']}
-        create_forecast.append(f_row)
+    elif event == 'Load Forecast':
+        nws = NWS(values['location'])
+        forecast = nws.standard()
+        window['nws'].update(values=forecast)
+    elif event == 'Add Day':
         window.extend_layout(window['column'], new_layout(i))
         i += 1
     elif event == 'Submit':
-        f_date = today + datetime.timedelta(days=i - 1)
-        f_row = {'created': today, 'date': f_date, 'temp_am': values['am-t'],
-                 'temp_pm': values['pm-t'], 'weather': values['weather']}
-        create_forecast.append(f_row)
-        break
-
+        am_temp = [values['am-t']]
+        pm_temp = [values['pm-t']]
+        weather = [values['weather']]
+        date = [today]
+        for k, v in values.items():
+            if isinstance(k, tuple):
+                if k[0] == 'date':
+                    date.append(v)
+                if k[0] == 'am-t':
+                    am_temp.append(v)
+                if k[0] == 'pm-t':
+                    pm_temp.append(v)
+                if k[0] == 'weather':
+                    weather.append(v)
+        create_forecast = [x for x in zip(date, am_temp, pm_temp, weather)]
+        window['output'].update(values=create_forecast)
+    elif event == 'Export':
+        with open(values['save_as'], 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Date', 'AM Temp', 'PM Temp', 'Weather'])
+            writer.writerows(create_forecast)
 event, values = window.read()
-print(create_forecast)
 window.close()
--- NWS.py
+++ NWS.py
@@ -1,6 +1,10 @@
 from noaa_sdk import NOAA
 from Geolocate import Geolocate
 from NWSDateParser import nws_date_parser
+from uszipcode import SearchEngine
+import datetime
+
+today = datetime.date.today()
 
 
 class NWS:
@@ -12,7 +16,40 @@ class NWS:
         self.lat = self.geoloc[0]
         self.lon = self.geoloc[1]
 
-    def forecasts(self):
+    def get_zip(self):
+        zip_search = SearchEngine()
+        city = self.location.split(',')[0].strip()
+        state = self.location.split(',')[1].strip()
+        try:
+            return zip_search.by_city_and_state(city, state)[0].zipcode
+        except Exception as e:
+            return e
+
+    def standard(self):
+        if isinstance(self.location, str):
+            zip = self.get_zip()
+        else:
+            zip = self.location
+        forecast_raw = self.nws.get_forecasts(zip, 'US', type='forecastHourly')
+        forecast_dict = {}
+        forecast = []
+        for d in range(8):
+            d_str = str(today + datetime.timedelta(days=d))
+            forecast_dict[d_str] = {}
+            for h in range(24):
+                forecast_dict[d_str][h] = {}
+        for row in forecast_raw:
+            date = row['startTime'][:10]
+            hour = int(row['startTime'][11:13])
+            temperature = row['temperature']
+            wind_speed = row['windSpeed']
+            wind_dir = row['windDirection']
+            wind = f'{wind_speed} {wind_dir}'
+            weather = row['shortForecast']
+            forecast.append((date, hour, temperature, wind, weather))
+        return forecast
+
+    def detailed(self):
         forecast_raw = self.nws.points_forecast(self.lat, self.lon, type='forecastGridData')
         forecast_dict = {}
         for property in forecast_raw['properties']:
@@ -60,7 +97,7 @@ if __name__ == '__main__':
     location = 75771
 
     nws = NWS(location)
-    f = nws.forecasts()
+    f = nws.standard()
     f_keys = list(f[0].keys())
     o = nws.observations()
     o_keys = list(f[0].keys())
