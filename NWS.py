2ce0b5aa713a61219f7d24fb3667f27d3fddf7b3
rbaker <rbaker@techservltd.net>
2025-01-02 08:26:47 -0600

    database changes

--- NWS.py
+++ NWS.py
@@ -152,8 +152,8 @@ def multi_city_forecasts(location_list: list, type: str = 'standard'):
 
 
 if __name__ == '__main__':
-    locations = ['Lindale, TX', 'Dallas, TX', 'Norman, OK']
-    forecasts = multi_city_forecasts(locations, 'standard')
+    locations = ['Lindale, TX']
+    forecasts, missing = multi_city_forecasts(locations, 'standard')
     # detailed = multi_city_forecasts(locations, 'detailed')
     # alerts = multi_city_forecasts(locations, 'alerts')
     # zones = get_zones()
---
+++ write_actual.sql
@@ -0,0 +1,2 @@
+INSERT INTO actual (location, timestamp, temperature)
+VALUES ('Lindale, TX', '2025-01-02 08:00:00', 35);
