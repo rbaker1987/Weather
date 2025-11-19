e8862826f59a49e12492ce5aca3b6f1275674ac2
rbaker <rbaker@techservltd.net>
2025-11-19 10:41:31 -0600

    Limit dashboard forecast to 3 cards, update logo to white, expand navbar logo, remove header text

--- weather_app/config/urls.py
+++ weather_app/config/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls')),
-    path('api/', include('weather.urls')),
 ]
 
 if settings.DEBUG:
